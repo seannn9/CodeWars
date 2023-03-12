22ac8846e6e0cc541ec3a0d3b40cd72454513c24
seannn9 <naeskirlu9@gmail.com>
2023-03-12 13:36:59 +0800

    Fixed code to continually check num until its single digit

--- C++/digital_root/digital_root.cpp
+++ C++/digital_root/digital_root.cpp
@@ -3,31 +3,27 @@
 using namespace std;
 
 // fix this so it will keep repeating until sum is one digit
-void digitalRoot(int num) {
+int digitalRoot(int num) {
     int digital;
     vector<int> arr;
-    while (num > 0) {
-        digital = num % 10;
-        num /= 10;
-        arr.push_back(digital);
+    while (num > 10) {
+        while (num > 0) {
+            digital = num % 10;
+            num /= 10;
+            arr.push_back(digital);
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            num += arr[i];
+        }
+        arr.clear();
     }
-    int sum = 0;
-    for (int i = 0; i < arr.size(); i++) {
-        sum += arr[i];
-    } cout << sum << endl;
-    arr.clear();
-    while (sum > 0) {
-        digital = sum%10;
-        sum /=10;
-        arr.push_back(digital);
-    }
-    sum = 0;
-    for (int i = 0; i < arr.size(); i++) {
-        sum += arr[i];
-    } cout << sum;
+    return num;
 }
 
 int main() {
-    digitalRoot(942);
+    int num;
+    cout << "Enter number to check its digital root: ";
+    cin >> num;
+    cout << digitalRoot(num) << endl;
     return 0;
 }
