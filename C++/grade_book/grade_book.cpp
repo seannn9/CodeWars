e10ce6e5ccf866138e8ac23d1e52df33466a3a37
seannn9 <naeskirlu9@gmail.com>
2023-03-12 23:44:04 +0800

    passed all tests in codewars

--- C++/grade_book/grade_book.cpp
+++ C++/grade_book/grade_book.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 
 char getGrade(int pre, int mid, int fin) {
-    char grade;
-    return grade;
-    
+    char letterGrade;
+    float score = (pre + mid + fin)/3;
+    if (score >= 90 && score <= 100) {
+        letterGrade = 'A';
+    } else if (score >= 80 && score < 90) {
+        letterGrade = 'B';
+    } else if (score >= 70 && score < 80) {
+        letterGrade = 'C';
+    } else if (score >= 60 && score < 70) {
+        letterGrade = 'D';
+    } else {
+        letterGrade = 'F';
+    }
+    return letterGrade;
 }
 
 int main() {
+    cout << getGrade(90, 85, 90) << endl;
     return 0;
 }
