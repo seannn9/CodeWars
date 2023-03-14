a29828fe0d1e89b45b37b6a252bc2a7a55f05d8a
seannn9 <naeskirlu9@gmail.com>
2023-03-14 23:53:21 +0800

    passed all tests in codewars

--- C++/armstrong_number/armstrong_number.cpp
+++ C++/armstrong_number/armstrong_number.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+#include<cmath>
 using namespace std;
 
+bool narcissistic(int num) {
+    vector<int> arr;
+    int root, sum = 0, count = 0, orig = num;
+    while (num > 0) {
+        root = num%10;
+        arr.push_back(root);
+        num/=10;
+        count++;
+    }
+    for (int i = 0; i < arr.size(); i++) {
+        sum += pow(arr[i], count);
+    }
+    if (sum == orig) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int main() {
+    cout << narcissistic(7);
     return 0;
 }
