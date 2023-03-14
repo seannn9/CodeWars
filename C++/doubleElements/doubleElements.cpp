7e70b7586b92ee19c626306cbacd2d4ebd7507c8
seannn9 <naeskirlu9@gmail.com>
2023-03-14 23:32:25 +0800

    Program working as intended (doubles each element of array)

--- C++/doubleElements/doubleElements.cpp
+++ C++/doubleElements/doubleElements.cpp
@@ -3,9 +3,19 @@
 using namespace std;
 
 vector<int> doubleEl(vector<int> arr) {
-
+    vector<int> newArr;
+    for (int i = 0; i < arr.size(); i++) {
+        arr[i] *= 2;
+        newArr.push_back(arr[i]);
+    }
+    return newArr;
 }
 
 int main() {
+    vector<int> arr = {4, 8, 12};
+    vector<int> newArr = doubleEl(arr);
+    for (int i = 0; i < arr.size(); i++) {
+        cout << newArr[i] << endl;
+    }
     return 0;
 }
